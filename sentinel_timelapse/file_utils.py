cf095a3cb0f37f95c43630a24f913b241188107e
ramaguirre <hotmail.ramaguirre@gmail.com>
2025-08-25 14:49:45 -0400

    Clean unused files

--- sentinel_timelapse/file_utils.py
+++
@@ -1,7 +0,0 @@
-import os
-
-def rename_clipped_files(rename_dic, folder='clipped_assets', prefix='ant_footprint_visual'):
-    for name_in in [os.path.join(folder, f) for f in os.listdir(folder) if f.endswith('.tif')]:
-        new_date = rename_dic[name_in.split('_')[-1].split('.')[0]]
-        name_out = os.path.join(folder, f'{prefix}_{new_date}.tif')
-        os.rename(name_in, name_out)
